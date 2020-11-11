cb629f31d76b3c99a75e5d321cfd3331ede7f765
Ouss Bahri <bahrioussama84@gmail.com>
2020-11-11 12:10:57 +0200

    adding functionalities to players buttons

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const p1Display = document.querySelector('#score1Display');
 const p2Display = document.querySelector('#score2Display');
 const button1 = document.querySelector('#p1Button');
 const button2 = document.querySelector('#p2Button');
-let score = 0;
+let p1score = 0;
+let p2score = 0;
+let winningScore=5;
+let isGameOver = false;
 
 button1.addEventListener('click', () => {
-   score += 1;
-   p1Display.textContent= score;
+    if(!isGameOver){
+   p1score += 1;
+   p1Display.textContent= p1score;
+    }
+    if (p1score === winningScore){
+        isGameOver= true;
+    }
 })
 button2.addEventListener('click', () => {
-   score += 1;
-   p2Display.textContent= score;
+    if(!isGameOver){
+        p2score += 1;
+        p2Display.textContent= p2score;
+         }
+         if (p2score === winningScore){
+             isGameOver= true;
+         }
 })
